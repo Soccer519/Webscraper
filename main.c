528182448824d1aab69c22e6a018a8e2372a5406
Mark Schill <maschill@iastate.edu>
2017-10-21 16:25:08 -0500

    Attempting to use realloc to create a new buffer size

--- main.c
+++ main.c
@@ -31,22 +31,39 @@ int setup_connection(const char *host, struct addrinfo *res) {
 	return sockfd;
 }
 
-void read_from_connection(int sockfd, char **buffer) {
-	char msg[] = "GET / HTTP/1.1\r\n\r\n";
+void read_from_connection(int sockfd, char **buffer, ssize_t buf_size) {
+	char msg[] = "GET /robots.txt HTTP/1.1\r\n\r\n";
 	ssize_t sent = send(sockfd, msg, sizeof(msg), 0);
 	if(sent < 0) {
 		fprintf(stderr, "send() failed\n");
 		exit(-1);
 	}
 
-	char buf[512];
-	ssize_t bytes = recv(sockfd, buf, sizeof(buf), 0);
-	if(bytes < 0) {
-		fprintf(stderr, "recv() failed\n");
-		exit(-1);
-	}
-	*buffer = malloc(sizeof(char) * strlen(buf));
-	strcpy(*buffer, buf);
+	char buf[buf_size];
+	char *ptr = malloc(buf_size);
+	ssize_t nbytes, totalbytes = 0;
+	do {
+		memset(buf, 0, buf_size);
+		nbytes = recv(sockfd, buf, buf_size, 0);
+		totalbytes += nbytes;
+		char *tmp_ptr = realloc(ptr, totalbytes);
+		if(ptr == NULL) {
+			printf("Done with something!\n");
+			free(tmp_ptr);
+		} else
+			ptr = tmp_ptr;
+		strcat(ptr, buf);
+	} while(nbytes > 0);
+	strcpy(*buffer, ptr);
+
+	// char buf[512];
+	// ssize_t bytes = recv(sockfd, buf, sizeof(buf), 0);
+	// if(bytes < 0) {
+	// 	fprintf(stderr, "recv() failed\n");
+	// 	exit(-1);
+	// }
+	// *buffer = malloc(sizeof(char) * strlen(buf));
+	// strcpy(*buffer, buf);
 }
 
 int main(void) {
@@ -62,13 +79,12 @@ int main(void) {
 		printf("Failed to connect to %s\n", hostname);
 
 
-	read_from_connection(sockfd, &html);
+	read_from_connection(sockfd, &html, 512);
 	printf("Read from the socket (%d): \n\n%s\n", strlen(html), html);
 	free(html);
-	
 
 	// Closing socket and freeing the linked list in res
-	// freeaddrinfo(res); // Apparently this doesn't have to be freed.... IDK
+	freeaddrinfo(res); // Apparently this doesn't have to be freed.... IDK
 	close(sockfd);
 	return 0;
 }
